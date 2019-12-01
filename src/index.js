65e35a216829622927f1e43dc7ba9ccde990522a
James Baker <bakerje@mac.com>
2019-12-01 14:12:49 +0000

    First entry added

--- src/index.js
+++ src/index.js
@@ -2,7 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  return <div>Hi</div>
+  return (
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            James
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 6pm</span>
+          </div>
+          <div className="text">Nice blog post</div>
+        </div>
+      </div>
+    </div>
+  )
+
+
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
