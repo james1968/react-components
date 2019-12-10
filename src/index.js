01d783f3ae93ad9935029cb51f7502e123d50984
James Baker <bakerje@mac.com>
2019-12-10 13:58:38 +0000

    Updated for props

--- src/index.js
+++ src/index.js
@@ -7,25 +7,30 @@ import ApprovalCard from './ApprovalCard';
 const App = () => {
   return (
     <div className="ui container comments">
-      <ApprovalCard/>
-      <CommentDetail
-        author="Sam"
-        timeAgo="Today at 4:45PM"
-        comment="Hi how are you?"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Tom"
-        timeAgo="Today at 11:15am"
-        comment="Hi how are you?"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Non"
-        timeAgo="Today at 3:35PM"
-        comment="Hi how are you?"
-        avatar={faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:45PM"
+          comment="Hi how are you?"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Tom"
+          timeAgo="Today at 11:15am"
+          comment="Hi how are you?"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Non"
+          timeAgo="Today at 3:35PM"
+          comment="Hi how are you?"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   )
 };
